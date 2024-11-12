e7b60426f283888f4c5a451d4dfa5a06b3f4de8d
Tunahan Ali Ozturk <72943708+Moongazing@users.noreply.github.com>
2024-11-12 16:04:14 +0300

    fix

--- src/Moongazing.Api/Program.cs
+++ src/Moongazing.Api/Program.cs
@@ -1,59 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Moongazing.ElasticSearch;
 using Moongazing.ElasticSearch.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ElasticSearchConfig>(builder.Configuration.GetSection("ElasticSearchConfig"));
+builder.Services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<ElasticSearchConfig>>().Value);
 builder.Services.AddSingleton<IElasticSearchService, ElasticSearchService>();
+
 var app = builder.Build();
 
-app.MapGet("/api/indexes", async (IElasticSearchService elasticSearchService) =>
+// Index listeleme
+app.MapGet("/api/indexes", async ([FromServices] IElasticSearchService elasticSearchService) =>
 {
     return Results.Ok(await Task.Run(() => elasticSearchService.GetIndexList()));
 });
 
-app.MapPost("/api/indexes", async (IElasticSearchService elasticSearchService, IndexModel model) =>
+// Yeni index oluşturma
+app.MapPost("/api/indexes", async ([FromServices] IElasticSearchService elasticSearchService, [FromBody] IndexModel model) =>
 {
     var result = await elasticSearchService.CreateNewIndexAsync(model);
     return Results.Ok(result);
 });
 
-app.MapPost("/api/documents/bulk", async (IElasticSearchService elasticSearchService, string indexName, object[] items) =>
+// Birçok belge ekleme (bulk insert)
+app.MapPost("/api/documents/bulk", async ([FromServices] IElasticSearchService elasticSearchService, [FromQuery] string indexName, [FromBody] object[] items) =>
 {
     var result = await elasticSearchService.InsertManyAsync(indexName, items);
     return Results.Ok(result);
 });
 
-app.MapDelete("/api/documents/{indexName}/{elasticId}", async (IElasticSearchService elasticSearchService, string indexName, string elasticId) =>
+// Belge silme
+app.MapDelete("/api/documents/{indexName}/{elasticId}", async ([FromServices] IElasticSearchService elasticSearchService, string indexName, string elasticId) =>
 {
     var model = new ElasticSearchModel { IndexName = indexName, ElasticId = elasticId };
     var result = await elasticSearchService.DeleteByElasticIdAsync(model);
     return Results.Ok(result);
 });
 
-app.MapGet("/api/documents/search", async (IElasticSearchService elasticSearchService, SearchParameters parameters) =>
+// Belge arama
+app.MapGet("/api/documents/search", async ([FromServices] IElasticSearchService elasticSearchService, [FromBody] SearchParameters parameters) =>
 {
     var result = await elasticSearchService.GetAllSearch<object>(parameters);
     return Results.Ok(result);
 });
 
-app.MapGet("/api/documents/searchByField", async (IElasticSearchService elasticSearchService, SearchByFieldParameters fieldParameters) =>
+// Alana göre arama
+app.MapGet("/api/documents/searchByField", async ([FromServices] IElasticSearchService elasticSearchService, [FromBody] SearchByFieldParameters fieldParameters) =>
 {
     var result = await elasticSearchService.GetSearchByField<object>(fieldParameters);
     return Results.Ok(result);
 });
 
-app.MapGet("/api/documents/searchByQuery", async (IElasticSearchService elasticSearchService, SearchByQueryParameters queryParameters) =>
+// Sorgu ile arama
+app.MapGet("/api/documents/searchByQuery", async ([FromServices] IElasticSearchService elasticSearchService, [FromBody] SearchByQueryParameters queryParameters) =>
 {
     var result = await elasticSearchService.GetSearchBySimpleQueryString<object>(queryParameters);
     return Results.Ok(result);
 });
 
-app.MapPost("/api/documents", async (IElasticSearchService elasticSearchService, ElasticSearchInsertUpdateModel model) =>
+// Tek bir belge ekleme
+app.MapPost("/api/documents", async ([FromServices] IElasticSearchService elasticSearchService, [FromBody] ElasticSearchInsertUpdateModel model) =>
 {
     var result = await elasticSearchService.InsertAsync(model);
     return Results.Ok(result);
 });
 
-app.MapPut("/api/documents/{indexName}/{elasticId}", async (IElasticSearchService elasticSearchService, string indexName, string elasticId, object item) =>
+// Belge güncelleme
+app.MapPut("/api/documents/{indexName}/{elasticId}", async ([FromServices] IElasticSearchService elasticSearchService, string indexName, string elasticId, [FromBody] object item) =>
 {
     var model = new ElasticSearchInsertUpdateModel { IndexName = indexName, ElasticId = elasticId, Item = item };
     var result = await elasticSearchService.UpdateByElasticIdAsync(model);
